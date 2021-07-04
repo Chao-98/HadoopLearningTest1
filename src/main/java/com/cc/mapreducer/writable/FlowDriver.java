fc51d64e845f04276e5e6405626f80e9c3bf526c
陈超 <cc98sept@qq.com>
2021-07-04 19:22:09 +0800

    20210704

--- src/main/java/com/cc/mapreducer/writable/FlowDriver.java
+++ src/main/java/com/cc/mapreducer/writable/FlowDriver.java
@@ -10,6 +10,14 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.io.IOException;
 
 /**
+ *
+ * 统计每一个手机号耗费的总上行流量、总下行流量、总流量
+ *
+ * 输入数据为phone_data.txt
+ * 输入格式为
+ * 7  13560436666  120.196.100.99  1116    954    200
+ * id 手机号码      网络 ip        上行流量 下行流量 网络状态码
+ *
  * 1.获取job
  * 2.设置jar包
  * 3.关联mapper和reducer
